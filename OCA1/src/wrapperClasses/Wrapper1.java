6280003cd0826c36ac3d5927895a1e1f337c5904
Lance Siddall <lancesiddall@live.co.uk>
2017-07-20 14:09:03 +0100

    Java Files
    I have added the Java files to GitHub

---
+++ OCA1/src/wrapperClasses/Wrapper1.java
@@ -0,0 +1,21 @@
+package wrapperClasses;
+/**
+Java defines a wrapper class for each of its primitive data types. The wrapper classes are used to wrap primitives in an object,
+ so they can be added to a collection object. They enable all types to be treated like object instances. 
+ Wrapper classes help you write cleaner code, which is easy to read. 
+ For this exam, you should be able to write code that uses these wrapper classes
+ */
+public class Wrapper1 {
+	//The Wrapper classes allows java to convert any primaitive data type into an object
+		public static void main(String[] args) {
+			int i = 5;
+			Integer iObj = new Integer(i);
+			System.out.println(iObj);
+			
+			
+			//Allows other variables to use the wrapper class content
+			int j = iObj.intValue();
+			System.out.println(j);
+		}
+
+	}
